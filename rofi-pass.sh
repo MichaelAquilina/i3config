6249f24931fb51eae31c7beaa4f2cedf927a82f0
Michael Aquilina <michaelaquilina@gmail.com>
2019-03-03 21:22:27 +0000

    Split password listing over multiple lines

--- rofi-pass.sh
+++ rofi-pass.sh
@@ -5,5 +5,7 @@ PASSWORD_STORE="$HOME/.password-store"
 if [[ -n "$1" ]]; then
     pass -c "$1" > /dev/null
 else
-    find "$PASSWORD_STORE" -name "*.gpg" | xargs realpath --relative-to="$PASSWORD_STORE" | sed "s/\.gpg//"
+    find "$PASSWORD_STORE" -name "*.gpg" \
+    -exec realpath --relative-to="$PASSWORD_STORE" {} +\
+    | sed "s/\.gpg//"
 fi
