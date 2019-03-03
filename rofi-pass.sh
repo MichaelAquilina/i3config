19a85582a6d886cd00442e2ee8749750c1944415
Michael Aquilina <michaelaquilina@gmail.com>
2019-03-03 21:16:14 +0000

    Add rofi pass script

---
+++ rofi-pass.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PASSWORD_STORE="$HOME/.password-store"
+
+if [[ -n "$1" ]]; then
+    pass -c "$1" > /dev/null
+else
+    find "$PASSWORD_STORE" -name "*.gpg" | xargs realpath --relative-to="$PASSWORD_STORE" | sed "s/\.gpg//"
+fi
